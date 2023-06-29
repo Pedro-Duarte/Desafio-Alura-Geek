0c3d1019f07d0a25c9c10c71b5218ddff3a9e68e
Pedro Souto Duarte Junior <pedro.duartejunior@gmail.com>
2023-06-28 21:46:39 -0300

    Preparando o ambiente

---
+++ services/produtos-services.js
@@ -0,0 +1,11 @@
+//GET
+
+const listaProdutos = () => {
+  fetch("http://localhost:3000/produtos")
+  .then(resposta => resposta.json())
+  .catch(error => console.log(error))
+}
+
+export const produtosServicos = {
+  listaProdutos
+}
